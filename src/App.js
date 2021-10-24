c94bdd01c1e95c2ee699133cb258a91017c82d84
ofadlon <orel.fadlon@Intel.com>
2021-10-24 19:19:45 +0300

    new error modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import { useState } from "react";
 import "./App.css";
-import AddUser from "./components/Users/AddUser.js";
+import ErrorModal from "./components/UI/ErrorModal";
+import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 function App() {
-  const [usersList, setUsersList] = useState([{ id: 0, userName: "Fadlon", age: 25 }]);
+  const [error, setError] = useState();
+  const [usersList, setUsersList] = useState([
+    { id: 0, userName: "Fadlon", age: 25 },
+  ]);
+
+  const showErrorHandler = (title, message) => {
+    setError({ title: title, message: message });
+  };
+
+  const closeErrorHandler = (event) => {
+    setError(null);
+  };
+
   const addUserHandler = (userName, age) => {
     setUsersList((prev) => {
       return [...prev, { id: prev.length, userName: userName, age: age }];
@@ -13,7 +26,14 @@ function App() {
 
   return (
     <div className="App">
-      <AddUser onAddUser={addUserHandler} />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={closeErrorHandler}
+        />
+      )}
+      <AddUser onAddUser={addUserHandler} onError={showErrorHandler} />
       <UsersList users={usersList} />
     </div>
   );
